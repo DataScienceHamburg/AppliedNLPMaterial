d9f6d8774f77de0330a795c8eff5ef933d953306
bert.gollnick <bert.gollnick@posteo.net>
2024-02-19 16:14:50 +0100

    data augmentation change on back translation

--- 100_DataAugmentation/020_BackTranslation.py
+++ 100_DataAugmentation/020_BackTranslation.py
@@ -10,8 +10,10 @@ original_en = 'The pen is mightier than the sword.'
 original_encoded = tokenizer(original_en, return_tensors="pt")
 generated_tokens = model.generate(**original_encoded, forced_bos_token_id=tokenizer.get_lang_id("de"))
 target_de = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+target_de
 # %% from target back to original language
 target_encoding = tokenizer(target_de, return_tensors="pt", padding=True)
 generated_tokens = model.generate(**target_encoding, forced_bos_token_id=tokenizer.get_lang_id("en"))
+
 tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
 # %%
--- 100_DataAugmentation/070_fill_mask.py
+++ 100_DataAugmentation/070_fill_mask.py
@@ -3,5 +3,5 @@ from transformers import pipeline
 
 #%%
 pipe = pipeline('fill-mask')
-pipe('This coffee is very <mask>.')
+pipe('The pen is <mask> than the sword.')
 # %%
